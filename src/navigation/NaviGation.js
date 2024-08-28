3ec9efd657b24b8c422b15136d29411b7b57b240
ledinhthanfpt <thanle.webdev@gmail.com>
2024-08-29 00:26:36 +0700

    update siderbar end header

--- src/navigation/NaviGation.js
+++ src/navigation/NaviGation.js
@@ -1,16 +1,22 @@
 import React from 'react';
 
 import Sidebar from '../components/Sidebar/Sidebar';
+import CustomHeader from '../components/Header/Header';
 
+import '../assets/styles/NaviGation.css';
 
 
 const NaviGation = () => {
   return (
-
-    <>
-      <Sidebar />
-    </>
-
+    <div className="layout-container">
+      <div className="sidebar-container">
+        <Sidebar />
+      </div>
+      <div className="main-container">
+        <CustomHeader />
+        {/* Nội dung khác có thể nằm dưới Header */}
+      </div>
+    </div>
   );
 };
 
